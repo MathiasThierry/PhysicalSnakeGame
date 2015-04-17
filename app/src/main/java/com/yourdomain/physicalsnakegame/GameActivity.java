c4f9a9f732496d20ad999b3ae68bee75a8212faf
MathiasThierry <Mathias@fthierry.dk>
2015-04-17 11:46:38 +0200

    seneste update

--- app/src/main/java/com/yourdomain/physicalsnakegame/GameActivity.java
+++ app/src/main/java/com/yourdomain/physicalsnakegame/GameActivity.java
@@ -14,7 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GameActivity extends MenuActivity
         implements OnMapReadyCallback{
-/*gg*/
+/*gg heufhewsfseifseihfesifbeshfbs*/
 
 GoogleMap myMap;
     @Override
